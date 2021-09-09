9cbc306f2423cae8e6dbc11859b78df5f3693358
Peer de Bakker <pde-bakk@student.codam.nl>
2021-09-09 11:17:20 +0200

    Finished part #2: https://www.youtube.com/watch?v=UDAVj_vlAr8

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
+  <div class="app">
+    <button @click="changeName('zelda')">change name</button>
+    <button @click="changeAge('35')">change age</button>
+  </div>
   <img alt="My logo" src="./assets/logo.png">
   <div class="justify-center flex bg-yellow-300 items-center h-screen">
     <div class="text-4xl">
       Hello 👋🏼
+      <p>{{ name }} - {{ age }}</p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
+  components: {},
+  data() {
+    return {
+      name: 'Link' as string,
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+    }
   }
 });
 </script>
