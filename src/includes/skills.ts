b6b15f26c8d8ae6ac024d1d3a1cf877a0fb347b5
Peer de Bakker <pde-bakk@student.codam.nl>
2021-09-10 16:31:24 +0200

    updated languages

--- src/includes/skills.ts
+++ src/includes/skills.ts
@@ -1,56 +1,101 @@
 export const programming = [
   {
+    name: 'C',
+    icon: 'c.png',
+    description:
+        'C was the first programming language I ever learnt, because the piscine and the first part of the 42 curriculum is done entirely in C. Knowing C has been useful to me for so much more than just writing C code: it makes learning new languages that much easier because everything is based on C.',
+    links: [
+      { name: 'Raytracer', link: 'https://github.com/pde-bakk/minirt' },
+      { name: 'Minishell', link: 'https://github.com/pde-bakk/minishell' },
+      { name: 'Push Swap', link: 'https://github.com/pde-bakk/push_swap' },
+    ],
+  },
+  {
+    name: 'Python',
+    icon: 'python.png',
+    description:
+        'From simple scripts to freelance job projects to datascience assignments, picking up Python has been a cakewalk and it\'s become a core foundation of my programming skills.',
+    links: [
+      { name: 'Datascience Logistic Regression', link: 'https://github.com/pde-bakk/DSLR' },
+      { name: 'Advent of Code', link: 'https://github.com/pde-bakk/AdventOfCode' },
+      { name: 'Bootcamp Python', link: 'https://github.com/pde-bakk/bootcamp_python' }
+    ],
+  },
+  {
+    name: 'C++',
+    icon: 'cplusplus.png',
+    description:
+        'Halfway through the core curriculum at 42, C takes a step backwards and swooping in comes C++ to teach us about templates, containers, writing our own webserver, and on Codingame it has become my goto for grapsh search algorithms <3.',
+    links: [{ name: 'Battleship', link: 'https://github.com/sandraroz/RiskWarzone_Game-1' }],
+  },
+  {
+    name: 'Ruby',
+    icon: 'ruby.png',
+    description:
+        'Ruby was the first scripting language I took up, because I had friends who loved it and I can see why. I just wish it was as widely used as Python so issues would be better google-able.',
+    links: [
+      { name: 'Cloudflight coding contest', link: 'https://github.com/pde-bakk/Cloudflight-coding-contest.git' },
+      { name: 'Ft_transcendence', link: 'https://github.com/pde-bakk/ft_transcendence.git' }
+    ],
+  },
+  {
+    name: 'VBA',
+    icon: 'vba.jpeg',
+    description:
+        'For my work at Amsterdam at Home Makelaars I had to do a lot of data querying in Microsoft Access and write macro\'s in Excel. VBA is an absolute lifesaver.'
+  },
+  {
+    name: 'Git',
+    icon: 'git.png',
+    description:
+        'I love Git. An absolute necessity in today\'s tech world.',
+    links: [
+      { name: 'Github', link: 'https://github.com/pde-bakk' }
+    ],
+  },
+  {
+    name: 'DevOps',
+    icon: 'devops.png',
+    description:
+        'For 42 projects such as ft_server and ft_services I had to dive into the world of DevOps with Dockerfiles and kubernetes. These skills still come in handy almost every day at work.',
+    links: [
+      { name: 'Ft_server', link: 'https://github.com/pde-bakk/ft_server.git' },
+      { name: 'Ft_services', link: 'https://github.com/pde-bakk/ft_services.git' }
+    ],
+  },
+  {
+    name: 'Rust',
+    icon: 'rust.png',
+    description:
+        'I love the safety of Rust while still maintaining the absolutely ridiculous speeds of C and C++. Therefore Rust has become my goto for Codingame competitions.',
+  },
+  {
     name: 'Typescript',
     icon: 'typescript.svg',
     description:
-      "This is my current favorite language. I've created a discord bot and this website with it. My final university project, a mobile tutoring application, also featured this language.",
+      'I love typescript, because it fixes all the issues I have with Javascript while keeping all the advantages.',
     links: [
-      { name: 'Tutor App', link: 'https://github.com/MinaSelim/Tutoring-app' },
       { name: 'Portfolio', link: 'https://github.com/sandraroz/portfolio' },
+    ],
+  },
+  {
+    name: 'Vue.js',
+    icon: 'vue.png',
+    description:
+        'Vue is something I decided to pick up recently. I\'ve been wanting to write my own personal website for a while now and I used this as practice for front-end related tasks at my job.',
+    links: [
+      { name: 'Portfolio', link: 'https://github.com/pde-bakk/personal-site.git' }
     ],
   },
   {
     name: 'Javascript',
     icon: 'javascript.png',
     description:
-      'Having worked with so many javascript frameworks, vanilla javascript is easy for me to code in. I made a music app following a Udemy course on vue.js.',
-    links: [{ name: 'Music Website', link: 'https://github.com/sandraroz/music-app' }],
-  },
-  {
-    name: 'Node.js',
-    icon: 'nodejs.png',
-    description:
-      'My two internships were with Node.js at Autodesk! I learned how to write scripts and modules. I had to rigorously test my code with unit and integration tests. This experience made me fall in love with the world of web development.',
-  },
-  {
-    name: 'Vue.js',
-    icon: 'vue.png',
-    description:
-      'I decided to improve on my front-end skills by taking a course on Udemy about vue. I enjoyed it a lot and decided to create this website as a result.',
+      'I wouldn\'t count javascript as one of my favourite languages since I love strongly typed languages, but it\'s been great to me for front-end projects. ',
     links: [
-      { name: 'Portfolio', link: 'https://github.com/sandraroz/portfolio' },
-      { name: 'Music Website', link: 'https://github.com/sandraroz/music-app' },
+        { name: 'Ft_transcendence', link: 'https://github.com/pde-bakk/ft_transcendence.git' },
+        { name: 'Portfolio', link: 'https://github.com/pde-bakk/personal-site.git' }
     ],
-  },
-  {
-    name: 'Java',
-    icon: 'java.png',
-    description:
-      'The first language they teach us in college is Java. Most of my classes used java. I participated in developing a navigation application for a college project.',
-    links: [{ name: 'Navigation App', link: 'https://github.com/sandraroz/SOEN390' }],
-  },
-  {
-    name: 'Python',
-    icon: 'python.png',
-    description:
-      'I took two AI classes that introduced me to machine learning and pattern recognition concepts. I therefore became familiar with the basics python.',
-  },
-  {
-    name: 'C++',
-    icon: 'cplusplus.png',
-    description:
-      'I took a class on C++ in college. It was a great way for me to understand some internal programming basics that can be expanded to any language. There was a project for the class about a cli battleship game that I had to develop.',
-    links: [{ name: 'Battleship', link: 'https://github.com/sandraroz/RiskWarzone_Game-1' }],
   },
 ];
 
