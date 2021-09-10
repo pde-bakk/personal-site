051e31ff61a73dcf9eccae080b7d3cbc7a4744fb
Peer de Bakker <pde-bakk@student.codam.nl>
2021-09-10 15:48:38 +0200

    changed text colour

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
     <div id="fancy" class="flex flex-col justify-center items-center h-48">
-      <h1>Hello, I'm <span class="text-pink-500">Peer de Bakker</span>.</h1>
-      <h1>I'm a software engineer.</h1>
+      <h1 class="text-white">Hello, I'm
+        <span class="text-pink-500">Peer de Bakker</span>
+        .
+      </h1>
+      <h1 class="text-white">I'm a software engineer.</h1>
     </div>
   </div>
 </template>
