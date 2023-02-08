3e80e6c650dd803c6d00c666eb7f9193b6f326ad
pde-bakk <pde-bakk@student.codam.nl>
2023-02-08 19:34:12 +0100

    added paragraphing to the text so it's less of a large blob

--- src/components/Codingame.jsx
+++ src/components/Codingame.jsx
@@ -9,6 +9,7 @@ import FantasticBits from '../assets/FantasticBits.jpg';
 const Codingame = () => {
 	return (
 		<div name='codingame' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+			<br/><br/><br/>
 			<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
 				<div className='pb-8 w-full flex justify-center items-center flex-col'>
 					<p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
@@ -18,24 +19,24 @@ const Codingame = () => {
 						In 2020, when Covid first hit, our entire education went remote.
 						This was a hard ask for all of us who loved to work on Campus and
 						rather struggled to motivate themselves to work from home.
-
+						<br/><br/>
 						It was then, that the 42 Network decided to organize a small bot programming contest
 						just for us. My friends and I loved it and put all our time in this.
-
+						<br/><br/>
 						This contest was hosted on codingame.com who just so happen to organize these kinds of contests
 						globally, twice a year.
 						Since then, I have never missed an edition. I'm always looking forward to these challenges for
 						months,
 						and when they are over I feel a bit empty inside.
-
+						<br/><br/>
 						Aside from just being tons of fun, this has helped me discover new interests in the tech field.
 						And for that, Codingame, I thank you.
-
+						<br/><br/>
 						They also offer a few other game-modes I'm enthralled with: Code golfing and Clash of Code.
 						Clash of Code is a 15 minute game, where everyone gets the same exercise and has to solve it
-						as fast as possible.
+						as fast as possible.<br/>
 						Code golfing is writing a solution to the problem in as few characters of code as possible.
-
+						<br/><br/>
 						Both of these game-modes I sucked at. I had friends who loved them, and reluctantly I gave them
 						a try.
 						And because I was pretty bad at them, I decided to grind it and now I'm top 100 in Clash of
