381c6c8f46db07b75c6f6b356a71a7ba13fa0a56
pde-bakk <pde-bakk@student.codam.nl>
2023-08-22 16:38:13 +0200

    removed comment

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from 'react-pdf';
-// import { Document, Page, pdfjs } from "react-pdf/dist/esm/entry.webpack5";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
