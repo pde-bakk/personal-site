61dd15ab97cbcb868ff279ab74599cc871400261
pde-bakk <pde-bakk@student.codam.nl>
2023-08-22 15:53:53 +0200

    idk anymore

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 // import { Document, Page } from 'react-pdf';
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import pdf_file from "./Kwartaalrapport_aah.pdf"
 
 const Work = () => {
-  const PDF_NAME = "./Kwartaalrapport_aah.pdf";
+  const PDF_NAME = pdf_file;
   // const [file, setFile] = useState(PDF_NAME);
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
