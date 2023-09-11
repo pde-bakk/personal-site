30ab2cbc2d146221562f95ccc3ec12561d0050b9
Restyled.io <commits@restyled.io>
2023-09-11 10:38:41 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -5,29 +5,33 @@ import {Route, Routes} from "react-router-dom";
 
 import About from "./components/About";
 import Codingame from "./components/Codingame";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
-import Footer from "./components/Footer";
 
 // https://blog.webdevsimplified.com/2022-07/react-router/
 
 ReactGA.initialize(process.env.GA_MEASUREMENT_ID);
 
 function App() {
-	return (
+        return (
 		<div>
 			<Navbar/>
 			<Routes>
-				<Route path="/" element={<Home/>}/>
+				<Route path="/" element={
+    <Home/>}/>
 				<Route path="/home" element={<Home/>}/>
-				<Route path="/about" element={<About/>}/>
+				<Route path="/about" element={
+    <About/>}/>
 				<Route path="/portfolio" element={<Portfolio/>}/>
-				<Route path="/codingame" element={<Codingame/>}/>
+				<Route path="/codingame" element={
+    <Codingame/>}/>
 				<Route path="/work" element={<Work/>}/>
-				<Route path="/skills" element={<Skills/>}/>
+				<Route path="/skills" element={
+    <Skills/>}/>
 				<Route path="/*" element={<Home/>}/>
 			</Routes>
 			<Footer/>
