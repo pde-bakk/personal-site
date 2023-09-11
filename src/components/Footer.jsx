9a714152f8c8598763f2304df156021d45789256
pde-bakk <pde-bakk@student.codam.nl>
2023-09-11 13:06:21 +0200

    added current year instead of hardcoding 2023

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
 		<footer
 			className="bg-footer_colour text-center lg:text-left">
 			<div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
-				© 2023 Copyright: Peer de Bakker
+				© {new Date().getFullYear()} Copyright: Peer de Bakker
 				{/*<a*/}
 				{/*	className="text-neutral-800 dark:text-neutral-400"*/}
 				{/*	href="https://tailwind-elements.com/"*/}
