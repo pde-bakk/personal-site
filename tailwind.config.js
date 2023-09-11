a767bd266ae5a236780490e95e6f949885453e45
pde-bakk <pde-bakk@student.codam.nl>
2023-09-11 02:13:42 +0200

    added custom colours to get rid of hard-to-read hex numbers in my code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background_colour': '#0a192f',
+        'footer_colour': '#09162a',
+        'almost_black': '#040c16',
+      }
+    },
   },
   plugins: [],
 }
