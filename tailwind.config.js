8005aae87ac345c2d00266abb06ffeffb89406b6
Restyled.io <commits@restyled.io>
2023-09-11 00:15:00 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'background_colour': '#0a192f',
-        'footer_colour': '#09162a',
-        'almost_black': '#040c16',
+  content :
+          [
+            "./src/**/*.{js,jsx,ts,tsx}",
+          ],
+  theme : {
+    extend : {
+      colors : {
+        'background_colour' : '#0a192f',
+        'footer_colour' : '#09162a',
+        'almost_black' : '#040c16',
       }
     },
   },
-  plugins: [],
+  plugins : [],
 }
