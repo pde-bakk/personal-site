6625bc212b9c90c6a96b9a1cdbd24833115bb9a9
pde-bakk <pde-bakk@student.codam.nl>
2023-09-13 00:25:05 +0200

    updated About page to use paragraphs, wowww

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,20 +17,22 @@ const About = () => {
 				</p>
 				<div className=" py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
 					<div className="max-w-[1000px] w-full text-center gap-8 px-4">
-						{/*<div>*/}
-							<p>
-								{" "}
-								I'm a software engineering student at 42Paris. I originally started off
-								at Codam, 42Paris' sister school in Amsterdam, the Netherlands.
-								Back in 2019, I did the piscine and it was there I wrote my first ever lines of code.
-								Since then, it's been a wild journey which has given me many new hobbies and skills.
-								<br/><br/>
+						<p>
+							I'm a Software Engineer, graduated from Codam and 42Paris.
+						</p>
+						<p>
+							Back in 2019, I did the piscine at Codam (Amsterdam) and it was there I wrote my first ever lines of code.
+							Since then, it's been a wild journey which has given me a wealth of hobbies and skills.
+							During these 2 years I've learnt many a thing about Software Engineering, Embedded Systems and Web Development (Front and Back).
+						</p>
+						<br/>
+						<p>
 								After finishing the Common Core (read: Bachelor), I was intrigued by AI and decided to make it my focus area.
-								Because I also believed that every engineer should have at least some knowledge about cybersecurity since it is becoming so important,
+								Because I also believed that every engineer should have at least some knowledge about Cyber Security (since it's becoming ever so important),
 								I went ahead and also dived into Binary Exploitation and Reverse Engineering.
-								That, combined with listening to many an episode of Darknet Diaries, has brought me to pursue a career in cybersecurity.
-							</p>
-						{/*</div>*/}
+								That, combined with listening to tons of Darknet Diaries, has brought me to now pursue a career in Cyber Security.
+						</p>
+
 					</div>
 				</div>
 			</div>
