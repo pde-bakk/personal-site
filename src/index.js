d13c4fcda8b160eb616bd06835db6e7d89138302
Restyled.io <commits@restyled.io>
2023-09-19 20:52:33 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
 import './i18n/config';
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
+
+import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
-);
+root.render(<React.StrictMode><BrowserRouter><App />
+            </BrowserRouter>
+	</React.StrictMode>);
