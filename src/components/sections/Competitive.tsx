dcf0c71577e428b73769a9f1fb42ed1150166383
pde-bakk <ptdebakker@gmail.com>
2026-02-12 22:12:34 +0100

    cover images bro

--- src/components/sections/Competitive.tsx
+++ src/components/sections/Competitive.tsx
@@ -2,127 +2,149 @@ import { useInView } from "../../hooks/useInView";
 
 const IMG_BASE = "https://static.codingame.com/servlet/fileservlet?id=";
 
-const botChallenges = [
-  {
-    title: "Soak Overflow",
-    subtitle: "Summer Challenge 2025",
-    ranking: 54,
-    total: 4111,
-    url: "https://www.codingame.com/multiplayer/bot-programming/soak-overflow",
-    cover: `${IMG_BASE}148057209217253`,
-  },
-  {
-    title: "Cellularena",
-    subtitle: "Winter Challenge 2024",
-    ranking: 23,
-    total: 4272,
-    url: "https://www.codingame.com/multiplayer/bot-programming/winter-challenge-2024",
-    cover: `${IMG_BASE}134969982703282`,
-  },
-  {
-    title: "Olymbits",
-    subtitle: "Summer Challenge 2024",
-    ranking: 413,
-    total: 6263,
-    url: "https://www.codingame.com/multiplayer/bot-programming/summer-challenge-2024-olymbits",
-    cover: `${IMG_BASE}125894403649811`,
-  },
-  {
-    title: "Seabed Security",
-    subtitle: "Fall Challenge 2023",
-    ranking: 408,
-    total: 5264,
-    url: "https://www.codingame.com/multiplayer/bot-programming/seabed-security",
-    cover: `${IMG_BASE}114650828445006`,
-  },
-  {
-    title: "Spring Challenge 2023",
-    subtitle: "Ants",
-    ranking: 758,
-    total: 5679,
-    url: "https://www.codingame.com/multiplayer/bot-programming/spring-challenge-2023-ants",
-    cover: `${IMG_BASE}103881564349131`,
-  },
-  {
-    title: "Keep Off The Grass",
-    subtitle: "Fall Challenge 2022",
-    ranking: 75,
-    total: 4979,
-    url: "https://www.codingame.com/multiplayer/bot-programming/keep-off-the-grass-fall-challenge-2022",
-    cover: `${IMG_BASE}91246287446747`,
-  },
-  {
-    title: "Spider Attack",
-    subtitle: "Spring Challenge 2022",
-    ranking: 483,
-    total: 9287,
-    url: "https://www.codingame.com/multiplayer/bot-programming/spring-challenge-2022",
-    cover: `${IMG_BASE}80263145171519`,
-  },
-  {
-    title: "Spring Challenge 2021",
-    subtitle: "",
-    ranking: 298,
-    total: 8438,
-    url: "https://www.codingame.com/multiplayer/bot-programming/spring-challenge-2021",
-    cover: `${IMG_BASE}64484028993851`,
-  },
-  {
-    title: "Fall Challenge 2020",
-    subtitle: "",
-    ranking: 431,
-    total: 8802,
-    url: "https://www.codingame.com/multiplayer/bot-programming/fall-challenge-2020",
-    cover: `${IMG_BASE}54137529209898`,
-  },
-  {
-    title: "Spring Challenge 2020",
-    subtitle: "",
-    ranking: 225,
-    total: 7589,
-    url: "https://www.codingame.com/multiplayer/bot-programming/spring-challenge-2020",
-    cover: `${IMG_BASE}44730303396746`,
-  },
-  {
-    title: "Hypersonic",
-    subtitle: "",
-    ranking: 263,
-    total: 3373,
-    url: "https://www.codingame.com/multiplayer/bot-programming/hypersonic",
-    cover: `${IMG_BASE}8231286622070`,
-  },
-  {
-    title: "Smash The Code",
-    subtitle: "",
-    ranking: 972,
-    total: 2958,
-    url: "https://www.codingame.com/multiplayer/bot-programming/smash-the-code",
-    cover: `${IMG_BASE}4769455388710`,
-  },
-  {
-    title: "Game of Drones",
-    subtitle: "",
-    ranking: 542,
-    total: 4606,
-    url: "https://www.codingame.com/multiplayer/bot-programming/game-of-drones",
-    cover: `${IMG_BASE}4769343092008`,
-  },
-  {
-    title: "Fantastic Bits",
-    subtitle: "",
-    ranking: 2209,
-    total: 4119,
-    url: "https://www.codingame.com/multiplayer/bot-programming/fantastic-bits",
-    cover: `${IMG_BASE}74038964843692`,
-  },
-  {
-    title: "Mad Pod Racing",
-    subtitle: "",
-    ranking: 6334,
-    total: 229432,
-    url: "https://www.codingame.com/multiplayer/bot-programming/mad-pod-racing",
-    cover: `${IMG_BASE}3825971694298`,
-  },
+class BotChallenge {
+  id: string;
+  title: string;
+  subtitle: string;
+  ranking: number;
+  total: number;
+  url: string;
+
+  constructor(id: string, title: string, subtitle: string, ranking: number, total: number, url: string) {
+    this.id = id;
+    this.title = title;
+    this.subtitle = subtitle;
+    this.ranking = ranking;
+    this.total = total;
+    this.url = url;
+  }
+
+  get cover() {
+    return `${IMG_BASE}${this.id}`;
+  }
+}
+
+const botChallenges: BotChallenge[] = [
+  new BotChallenge(
+      "148057209217253",
+      "Soak Overflow",
+      "Summer Challenge 2025",
+      54,
+      4111,
+      "https://www.codingame.com/multiplayer/bot-programming/soak-overflow"
+  ),
+  new BotChallenge(
+      "134969982703282",
+      "Cellularena",
+      "Winter Challenge 2024",
+      23,
+      4272,
+      "https://www.codingame.com/multiplayer/bot-programming/winter-challenge-2024"
+  ),
+  new BotChallenge(
+      "125894403649811",
+      "Olymbits",
+      "Summer Challenge 2024",
+      413,
+      6263,
+      "https://www.codingame.com/multiplayer/bot-programming/summer-challenge-2024-olymbits"
+  ),
+  new BotChallenge(
+      "114650828445006",
+      "Seabed Security",
+      "Fall Challenge 2023",
+      408,
+      5264,
+      "https://www.codingame.com/multiplayer/bot-programming/seabed-security"
+  ),
+  new BotChallenge(
+      "103881564349131",
+      "Spring Challenge 2023",
+      "Ants",
+      758,
+      5679,
+      "https://www.codingame.com/multiplayer/bot-programming/spring-challenge-2023-ants"
+  ),
+  new BotChallenge(
+      "91246287446747",
+      "Keep Off The Grass",
+      "Fall Challenge 2022",
+      75,
+      4979,
+      "https://www.codingame.com/multiplayer/bot-programming/keep-off-the-grass-fall-challenge-2022"
+  ),
+  new BotChallenge(
+      "80263145171519",
+      "Spider Attack",
+      "Spring Challenge 2022",
+      483,
+      9287,
+      "https://www.codingame.com/multiplayer/bot-programming/spring-challenge-2022"
+  ),
+  new BotChallenge(
+      "64484028993851",
+      "Spring Challenge 2021",
+      "",
+      298,
+      8438,
+      "https://www.codingame.com/multiplayer/bot-programming/spring-challenge-2021"
+  ),
+  new BotChallenge(
+      "54137529209898",
+      "Fall Challenge 2020",
+      "",
+      431,
+      8802,
+      "https://www.codingame.com/multiplayer/bot-programming/fall-challenge-2020"
+  ),
+  new BotChallenge(
+      "44730303396746",
+      "Spring Challenge 2020",
+      "",
+      225,
+      7589,
+      "https://www.codingame.com/multiplayer/bot-programming/spring-challenge-2020"
+  ),
+  new BotChallenge(
+      "8231286622070",
+      "Hypersonic",
+      "",
+      263,
+      3373,
+      "https://www.codingame.com/multiplayer/bot-programming/hypersonic"
+  ),
+  new BotChallenge(
+      "4769455388710",
+      "Smash The Code",
+      "",
+      972,
+      2958,
+      "https://www.codingame.com/multiplayer/bot-programming/smash-the-code"
+  ),
+  new BotChallenge(
+      "4769343092008",
+      "Game of Drones",
+      "",
+      542,
+      4606,
+      "https://www.codingame.com/multiplayer/bot-programming/game-of-drones"
+  ),
+  new BotChallenge(
+      "74038964843692",
+      "Fantastic Bits",
+      "",
+      2209,
+      4119,
+      "https://www.codingame.com/multiplayer/bot-programming/fantastic-bits"
+  ),
+  new BotChallenge(
+      "3825971694298",
+      "Mad Pod Racing",
+      "",
+      6334,
+      229432,
+      "https://www.codingame.com/multiplayer/bot-programming/mad-pod-racing"
+  ),
 ];
 
 const formatPercentile = (ranking: number, total: number) => {
