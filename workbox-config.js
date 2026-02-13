d542ccaa015ae2ffa1fcc772792042b0d2f06666
pde-bakk <ptdebakker@gmail.com>
2026-02-13 11:48:59 +0100

    wo

--- workbox-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: "build/",
-  globPatterns: ["**/*.{json,ico,html,pdf,txt,css,js,jpg,png}"],
-  swDest: "build/sw.js",
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-};
